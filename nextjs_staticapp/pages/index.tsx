b874450c4fe8f3d959e3bc0c2f77c29e5dad26fc
Justine <ujusine@yahoo.com>
2022-11-22 23:54:33 +0800

    render App

--- nextjs_staticapp/pages/index.tsx
+++ nextjs_staticapp/pages/index.tsx
@@ -1,16 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Globals.module.css";
-import Home from "../components/Home";
-import Navbar from "../components/Navbar";
-import AboutDev from "../components/AboutDev";
+import App from "../components/App";
 
 export default function Index() {
-  return (
-    <div className={styles.padd}>
-      <Navbar />
-      <Home />
-      <AboutDev />
-    </div>
-  );
+  return <App />;
 }
